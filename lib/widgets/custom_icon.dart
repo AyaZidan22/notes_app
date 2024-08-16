fc52c27e0e372c0a91a155e9c03d68be89a39485
Aya Zidan <ayazedang1@gmail.com>
2024-08-16 20:17:48 +0300

    custom app bar

---
+++ lib/widgets/custom_icon.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class CustomIcon extends StatelessWidget {
+  const CustomIcon({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 45,
+      height: 45,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(16),
+        color: Colors.white.withOpacity(0.2),
+      ),
+      child: IconButton(
+        onPressed: () {},
+        icon: const Icon(Icons.search),
+      ),
+    );
+  }
+}
